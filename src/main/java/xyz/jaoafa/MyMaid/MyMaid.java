fc0082b5b03148e2f541dc34e61938858d33dad3
tomachi <tomachi@ICHIGO>
2016-04-04 16:25:04 +0900

    First Commit

---
+++ src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
@@ -0,0 +1,15 @@
+package xyz.jaoafa.MyMaid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyMaid extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
