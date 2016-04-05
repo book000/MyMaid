75c53dbca99068a6e1155342d642d0d17d72db48
tomachi <lego@aol.jp>
2016-04-05 17:05:36 +0900

    onEnable edited

--- src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
+++ src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
@@ -5,7 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MyMaid extends JavaPlugin {
     @Override
     public void onEnable() {
-    	
+    	getLogger().info("(c) jao Minecraft Server MyMaid Project.");
+    	getLogger().info("Product by tomachi.");
     }
 
     @Override
