51b2c9ebe82d9d6d768884e08369aa85325a2e4d
tomachi <lego@aol.jp>
2016-04-05 16:57:33 +0900

    recreate

--- src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
+++ src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
@@ -3,13 +3,13 @@ package xyz.jaoafa.mymaid;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyMaid extends JavaPlugin {
-    @Override
+	@Override
     public void onEnable() {
-    	
+        // TODO ここに、プラグインが有効化された時の処理を実装してください。
     }
 
     @Override
     public void onDisable() {
-
+        // TODO ここに、プラグインが無効化された時の処理を実装してください。
     }
 }
