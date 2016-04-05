fec493324666593a26875095571ec8b5a097643f
tomachi <lego@aol.jp>
2016-04-05 17:13:23 +0900

    comment add

--- src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
+++ src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
@@ -1,9 +1,15 @@
 package xyz.jaoafa.mymaid;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyMaid extends JavaPlugin {
-    @Override
+	/** 設定ファイルの文字コード（UTF-8の場合） */
+	static final private Charset CONFIG_CHAREST=StandardCharsets.UTF_8;
+
+	@Override
     public void onEnable() {
     	getLogger().info("(c) jao Minecraft Server MyMaid Project.");
     	getLogger().info("Product by tomachi.");
@@ -13,4 +19,26 @@ public class MyMaid extends JavaPlugin {
     public void onDisable() {
 
     }
+
+    /*
+     *
+     * // プラグインフォルダ上の設定ファイルのパスを作る
+		String confFilePath = "./plugins/dynmap/markers.yml";
+
+		// 設定ファイルを開く
+		try(Reader reader=new InputStreamReader(new FileInputStream(confFilePath),CONFIG_CHAREST)){
+
+			// 設定データ入出力クラスを作る
+			FileConfiguration conf=new YamlConfiguration();
+
+			// 設定を読み込む
+			conf.load(reader);
+		}catch(Exception e){
+			// エラーが起きた場合はその旨を表示する
+			System.out.println(e);
+
+			// 起動失敗のためこのプラグインを無効にする
+			onDisable();
+		}
+     */
 }
