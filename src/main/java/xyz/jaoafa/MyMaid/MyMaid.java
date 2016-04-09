f9e398c929e0845c13099585ed5d87702dcd8303
tomachi <lego@aol.jp>
2016-04-09 21:46:40 +0900

    Revert "jf導入"
    This reverts commit 7b8f25e2540146e0e0b4855c6c40451ba891756e.

--- src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
+++ src/main/java/xyz/jaoafa/MyMaid/MyMaid.java
@@ -1,24 +1,24 @@
 package xyz.jaoafa.mymaid;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 public class MyMaid extends JavaPlugin {
-	BukkitTask task = null;//あとで自分で自分を止めるためのもの
+<<<<<<< HEAD
+	@Override
+    public void onEnable() {
+        // TODO ここに、プラグインが有効化された時の処理を実装してください。
+=======
+	/** 設定ファイルの文字コード（UTF-8の場合） */
+	static final private Charset CONFIG_CHAREST=StandardCharsets.UTF_8;
+
 	@Override
     public void onEnable() {
     	getLogger().info("(c) jao Minecraft Server MyMaid Project.");
     	getLogger().info("Product by tomachi.");
-
+>>>>>>> refs/remotes/origin/tomachi
     }
 
     @Override
@@ -26,35 +26,25 @@ public class MyMaid extends JavaPlugin {
         // TODO ここに、プラグインが無効化された時の処理を実装してください。
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-        // プレイヤーが「/basic」コマンドを投入した際の処理...
-    	if(cmd.getName().equalsIgnoreCase("jf")){
-    		Player player = (Player) sender;
-    		Date date1 = new Date();
-    		SimpleDateFormat date = new SimpleDateFormat("H:m:s");
-    		Bukkit.broadcastMessage(ChatColor.GRAY + "[" + date.format(date1) +"]"  + ChatColor.WHITE + player.getName() +  ": jao");
-    		task = this.getServer().getScheduler().runTaskTimer(this, new Timer(this, player.getName()), 60L, 60L);
-    		return true;
-		}
-    	return false;
-        // コマンドが実行されなかった場合は、falseを返して当メソッドを抜ける。
-    }
+    /*
+     *
+     * // プラグインフォルダ上の設定ファイルのパスを作る
+		String confFilePath = "./plugins/dynmap/markers.yml";
 
-    private class Timer extends BukkitRunnable{
-		String user;//秒数
-		JavaPlugin plugin;//BukkitのAPIにアクセスするためのJavaPlugin
-		public Timer(JavaPlugin plugin, String user) {
-			this.plugin = plugin;
-			this.user = user;
-		}
-		@Override
-		public void run() {
-			Date date1 = new Date();
-			SimpleDateFormat date = new SimpleDateFormat("H:m:s");
-			Bukkit.broadcastMessage(ChatColor.GRAY + "["+ date.format(date1) + "]" + ChatColor.WHITE + user +  ": afa");
-			task.cancel();
-		}
-	}
+		// 設定ファイルを開く
+		try(Reader reader=new InputStreamReader(new FileInputStream(confFilePath),CONFIG_CHAREST)){
 
+			// 設定データ入出力クラスを作る
+			FileConfiguration conf=new YamlConfiguration();
+
+			// 設定を読み込む
+			conf.load(reader);
+		}catch(Exception e){
+			// エラーが起きた場合はその旨を表示する
+			System.out.println(e);
+
+			// 起動失敗のためこのプラグインを無効にする
+			onDisable();
+		}
+     */
 }
