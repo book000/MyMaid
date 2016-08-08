4bfe93285254e5350080a73836b59befdb119abe
tomachi <lego@aol.jp>
2016-08-08 09:09:28 +0900

    skkのTabList導入、そのほか修正など

--- src/main/java/xyz/jaoafa/mymaid/Command/SSK.java
+++ src/main/java/xyz/jaoafa/mymaid/Command/SSK.java
@@ -3,6 +3,7 @@ package xyz.jaoafa.mymaid.Command;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -24,20 +25,90 @@ public class SSK implements CommandExecutor {
 	  		String data = Method.url_jaoplugin("joinvote", "u="+uuid);
 	  		String[] arr = data.split("###", 0);
 	  		MyMaid.chatcolor.put(player.getName(), arr[1]);
+			if(player.hasPermission("mymaid.pex.limited")){
+				player.setPlayerListName(ChatColor.BLACK + "■" + ChatColor.WHITE + player.getName());
+			}else if(Prison.prison.containsKey(player.getName())){
+				player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.WHITE + player.getName());
+			}else if(MyMaid.chatcolor.containsKey(player.getName())){
+				int i = Integer.parseInt(MyMaid.chatcolor.get(player.getName()));
+				if(i >= 0 && i <= 5){
+					player.setPlayerListName(ChatColor.WHITE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 6 && i <= 19){
+					player.setPlayerListName(ChatColor.DARK_BLUE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 20 && i <= 33){
+					player.setPlayerListName(ChatColor.BLUE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 34 && i <= 47){
+					player.setPlayerListName(ChatColor.AQUA + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 48 && i <= 61){
+					player.setPlayerListName(ChatColor.DARK_AQUA  + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 62 && i <= 76){
+					player.setPlayerListName(ChatColor.DARK_GREEN + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 77 && i <= 89){
+					player.setPlayerListName(ChatColor.GREEN + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 90 && i <= 103){
+					player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.YELLOW + player.getName());
+				}else if(i >= 104 && i <= 117){
+					player.setPlayerListName(ChatColor.GOLD + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 118 && i <= 131){
+					player.setPlayerListName(ChatColor.RED + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 132 && i <= 145){
+					player.setPlayerListName(ChatColor.DARK_RED + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 146 && i <= 159){
+					player.setPlayerListName(ChatColor.DARK_PURPLE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 160){
+					player.setPlayerListName(ChatColor.LIGHT_PURPLE + "■" + ChatColor.WHITE + player.getName());
+				}
+			}else{
+				player.setPlayerListName(ChatColor.GRAY + "■" + ChatColor.WHITE + player.getName());
+			}
 	  		Method.SendMessage(sender, cmd, player.getName()+"'s SKK -> "+arr[1]);
 		}else if(args.length == 1){
-			for(Player p: Bukkit.getServer().getOnlinePlayers()) {
-				if(p.getName().equalsIgnoreCase(args[0])){
-					UUID uuid = p.getUniqueId();
+			for(Player player: Bukkit.getServer().getOnlinePlayers()) {
+				if(player.getName().equalsIgnoreCase(args[0])){
+					UUID uuid = player.getUniqueId();
 			  		String data = Method.url_jaoplugin("joinvote", "u="+uuid);
 			  		String[] arr = data.split("###", 0);
-			  		MyMaid.chatcolor.put(p.getName(), arr[1]);
-			  		Method.SendMessage(sender, cmd, p.getName()+"'s SKK -> "+arr[1]);
+			  		MyMaid.chatcolor.put(player.getName(), arr[1]);
+					if(player.hasPermission("mymaid.pex.limited")){
+						player.setPlayerListName(ChatColor.BLACK + "■" + ChatColor.WHITE + player.getName());
+					}else if(Prison.prison.containsKey(player.getName())){
+						player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.WHITE + player.getName());
+					}else if(MyMaid.chatcolor.containsKey(player.getName())){
+						int i = Integer.parseInt(MyMaid.chatcolor.get(player.getName()));
+						if(i >= 0 && i <= 5){
+							player.setPlayerListName(ChatColor.WHITE + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 6 && i <= 19){
+							player.setPlayerListName(ChatColor.DARK_BLUE + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 20 && i <= 33){
+							player.setPlayerListName(ChatColor.BLUE + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 34 && i <= 47){
+							player.setPlayerListName(ChatColor.AQUA + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 48 && i <= 61){
+							player.setPlayerListName(ChatColor.DARK_AQUA  + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 62 && i <= 76){
+							player.setPlayerListName(ChatColor.DARK_GREEN + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 77 && i <= 89){
+							player.setPlayerListName(ChatColor.GREEN + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 90 && i <= 103){
+							player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.YELLOW + player.getName());
+						}else if(i >= 104 && i <= 117){
+							player.setPlayerListName(ChatColor.GOLD + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 118 && i <= 131){
+							player.setPlayerListName(ChatColor.RED + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 132 && i <= 145){
+							player.setPlayerListName(ChatColor.DARK_RED + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 146 && i <= 159){
+							player.setPlayerListName(ChatColor.DARK_PURPLE + "■" + ChatColor.WHITE + player.getName());
+						}else if(i >= 160){
+							player.setPlayerListName(ChatColor.LIGHT_PURPLE + "■" + ChatColor.WHITE + player.getName());
+						}
+					}else{
+						player.setPlayerListName(ChatColor.GRAY + "■" + ChatColor.WHITE + player.getName());
+					}
+			  		Method.SendMessage(sender, cmd, player.getName()+"'s SKK -> "+arr[1]);
 				}
     		}
-
 		}
-
 		return true;
 	}
 }
--- src/main/java/xyz/jaoafa/mymaid/EventHandler/OnPlayerJoinEvent.java
+++ src/main/java/xyz/jaoafa/mymaid/EventHandler/OnPlayerJoinEvent.java
@@ -6,21 +6,24 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import xyz.jaoafa.mymaid.Method;
 import xyz.jaoafa.mymaid.MyMaid;
+import xyz.jaoafa.mymaid.Command.Prison;
 
 public class OnPlayerJoinEvent implements Listener {
 	JavaPlugin plugin;
 	public OnPlayerJoinEvent(JavaPlugin plugin) {
 		this.plugin = plugin;
 	}
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
   		if(Bukkit.getServer().getOnlinePlayers().size() >= 1 && !MyMaid.nextbakrender){
   			MyMaid.nextbakrender = true;
@@ -39,5 +42,53 @@ public class OnPlayerJoinEvent implements Listener {
   		Date Date = new Date();
 		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 		Bukkit.broadcastMessage(ChatColor.GRAY + "["+ timeFormat.format(Date) + "]" + ChatColor.GRAY + "■" + ChatColor.WHITE + "jaotan: 現在『" + Bukkit.getServer().getOnlinePlayers().size() + "人』がログインしています");
+		Player player = event.getPlayer();
+		new TabListSKKReloader(plugin, player).runTaskLater(plugin, 10L);
+
   	}
+	private class TabListSKKReloader extends BukkitRunnable{
+    	Player player;
+    	public TabListSKKReloader(JavaPlugin plugin, Player player) {
+    		this.player = player;
+    	}
+		@Override
+		public void run() {
+			if(player.hasPermission("mymaid.pex.limited")){
+				player.setPlayerListName(ChatColor.BLACK + "■" + ChatColor.WHITE + player.getName());
+			}else if(Prison.prison.containsKey(player.getName())){
+				player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.WHITE + player.getName());
+			}else if(MyMaid.chatcolor.containsKey(player.getName())){
+				int i = Integer.parseInt(MyMaid.chatcolor.get(player.getName()));
+				if(i >= 0 && i <= 5){
+					player.setPlayerListName(ChatColor.WHITE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 6 && i <= 19){
+					player.setPlayerListName(ChatColor.DARK_BLUE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 20 && i <= 33){
+					player.setPlayerListName(ChatColor.BLUE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 34 && i <= 47){
+					player.setPlayerListName(ChatColor.AQUA + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 48 && i <= 61){
+					player.setPlayerListName(ChatColor.DARK_AQUA  + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 62 && i <= 76){
+					player.setPlayerListName(ChatColor.DARK_GREEN + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 77 && i <= 89){
+					player.setPlayerListName(ChatColor.GREEN + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 90 && i <= 103){
+					player.setPlayerListName(ChatColor.DARK_GRAY + "■" + ChatColor.YELLOW + player.getName());
+				}else if(i >= 104 && i <= 117){
+					player.setPlayerListName(ChatColor.GOLD + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 118 && i <= 131){
+					player.setPlayerListName(ChatColor.RED + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 132 && i <= 145){
+					player.setPlayerListName(ChatColor.DARK_RED + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 146 && i <= 159){
+					player.setPlayerListName(ChatColor.DARK_PURPLE + "■" + ChatColor.WHITE + player.getName());
+				}else if(i >= 160){
+					player.setPlayerListName(ChatColor.LIGHT_PURPLE + "■" + ChatColor.WHITE + player.getName());
+				}
+			}else{
+				player.setPlayerListName(ChatColor.GRAY + "■" + ChatColor.WHITE + player.getName());
+			}
+		}
+	}
 }
