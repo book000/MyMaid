96372e5fec9dacc8c2c4a712d5629ae99fec1823
tomachi <lego@aol.jp>
2017-03-12 21:45:43 +0900

    add: 予約済み変数の追加

--- src/main/java/xyz/jaoafa/mymaid/Command/VarCmd.java
+++ src/main/java/xyz/jaoafa/mymaid/Command/VarCmd.java
@@ -1,5 +1,7 @@
 package xyz.jaoafa.mymaid.Command;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -32,9 +34,31 @@ public class VarCmd implements CommandExecutor {
 			c++;
 		}
 		text = StringUtils.stripStart(text, "/");
+
+		// ----- 事前定義(予約済み変数) ----- //
+
+		SimpleDateFormat sdf_Year = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Year" + "\\$", sdf_Year.format(new Date()));
+		SimpleDateFormat sdf_Month = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Month" + "\\$", sdf_Month.format(new Date()));
+		SimpleDateFormat sdf_Day = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Day" + "\\$", sdf_Day.format(new Date()));
+
+		SimpleDateFormat sdf_Hour = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Hour" + "\\$", sdf_Hour.format(new Date()));
+		SimpleDateFormat sdf_Minute = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Minute" + "\\$", sdf_Minute.format(new Date()));
+		SimpleDateFormat sdf_Second = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		text = text.replaceAll("\\$" + "DateTime_Second" + "\\$", sdf_Second.format(new Date()));
+
+		text = text.replaceAll("\\$" + "PlayerCount" + "\\$", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
+
+		// ----- 事前定義(予約済み変数) ----- //
+
 		for(Map.Entry<String, String> e : Var.var.entrySet()) {
 			text = text.replaceAll("\\$" + e.getKey() + "\\$", e.getValue());
 		}
+
 		Bukkit.dispatchCommand(sender, text);
 		Method.SendMessage(sender, cmd, "コマンド「" + text + "」を実行しました。");
 		return true;
