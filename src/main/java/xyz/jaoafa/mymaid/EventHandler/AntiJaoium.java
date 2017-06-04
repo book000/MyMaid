d684cc94839294cd7e37fe8792fca25a39f72f39
tomachi <lego@aol.jp>
2017-06-04 16:27:33 +0900

    change: Antijaoium改定

--- src/main/java/xyz/jaoafa/mymaid/EventHandler/AntiJaoium.java
+++ src/main/java/xyz/jaoafa/mymaid/EventHandler/AntiJaoium.java
@@ -1,5 +1,7 @@
 package xyz.jaoafa.mymaid.EventHandler;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -7,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -17,6 +20,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import xyz.jaoafa.mymaid.Jail.Jail;
 
@@ -25,6 +29,53 @@ public class AntiJaoium implements Listener {
 	public AntiJaoium(JavaPlugin plugin) {
 		this.plugin = plugin;
 	}
+	/**
+	 * jaoiumと判定されるアイテムかどうか
+	 * @param list PotionEffectのList
+	 * @return jaoiumかどうか
+	 * @author mine_book000
+	 */
+	private boolean isjaoium(List<PotionEffect> list){
+		Boolean jaoium = false;
+		for (PotionEffect po : list) {
+			if(po.getType() == PotionEffectType.HEAL){
+				if(po.getAmplifier() == 29){
+					// アウト
+					jaoium = true;
+				}
+				if(po.getAmplifier() == 125){
+					// アウト
+					jaoium = true;
+				}
+			}
+			if(po.getType() == PotionEffectType.HEALTH_BOOST){
+				if(po.getAmplifier() == 249){
+					// アウト
+					jaoium = true;
+				}
+			}
+		}
+		return jaoium;
+	}
+	/**
+	 * jaoiumと判定されるアイテムかどうか(ディスペンサーで放ってよいかどうか)
+	 * @param list PotionEffectのList
+	 * @return jaoiumかどうか
+	 * @author mine_book000
+	 */
+	private boolean isjaoium_indispenser(List<PotionEffect> list){
+		Boolean jaoium = false;
+		for (PotionEffect po : list) {
+			if(po.getType() == PotionEffectType.HEALTH_BOOST){
+				if(po.getAmplifier() == 249){
+					// アウト
+					jaoium = true;
+				}
+			}
+		}
+		return jaoium;
+	}
+
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void InvClick(InventoryClickEvent event) {
@@ -35,6 +86,9 @@ public class AntiJaoium implements Listener {
 		Inventory inventory = event.getInventory();
 		Inventory clickedinventory = event.getClickedInventory();
 		ItemStack[] is = inventory.getContents();
+		if(Jail.isJail(player)){
+			return;
+		}
 		Boolean jaoium = false;
 		for(int n=0; n != is.length; n++)
 		{
@@ -44,21 +98,9 @@ public class AntiJaoium implements Listener {
 			ItemStack hand = is[n];
 			if(hand.getType() == Material.POTION){
 				PotionMeta potion = (PotionMeta) hand.getItemMeta();
-				for (PotionEffect po : potion.getCustomEffects()) {
-					if(Jail.isJail(player)){
-						return;
-					}
-					if(po.getAmplifier() == 29){
-						// アウト
-						jaoium = true;
-					}
-					if(po.getAmplifier() == 125){
-						// アウト
-						jaoium = true;
-					}
-					if(jaoium){
-						inventory.clear(n);
-					}
+				jaoium = isjaoium(potion.getCustomEffects());
+				if(jaoium){
+					inventory.clear(n);
 				}
 			}
 		}
@@ -75,23 +117,10 @@ public class AntiJaoium implements Listener {
 				ItemStack hand = is[n];
 				if(hand.getType() == Material.POTION){
 					PotionMeta potion = (PotionMeta) hand.getItemMeta();
-					for (PotionEffect po : potion.getCustomEffects()) {
-						if(Jail.isJail(player)){
-							return;
-						}
-						if(po.getAmplifier() == 29){
-							// アウト
-							jaoium = true;
-						}
-						if(po.getAmplifier() == 125){
-							// アウト
-							jaoium = true;
-						}
-						if(jaoium){
-							clickedinventory.clear(n);
-						}
+					jaoium = isjaoium(potion.getCustomEffects());
+					if(jaoium){
+						inventory.clear(n);
 					}
-
 				}
 			}
 			if(jaoium){
@@ -119,21 +148,9 @@ public class AntiJaoium implements Listener {
 			ItemStack hand = is[n];
 			if(hand.getType() == Material.POTION){
 				PotionMeta potion = (PotionMeta) hand.getItemMeta();
-				for (PotionEffect po : potion.getCustomEffects()) {
-					if(Jail.isJail(player)){
-						return;
-					}
-					if(po.getAmplifier() == 29){
-						// アウト
-						jaoium = true;
-					}
-					if(po.getAmplifier() == 125){
-						// アウト
-						jaoium = true;
-					}
-					if(jaoium){
-						inventory.clear(n);
-					}
+				jaoium = isjaoium(potion.getCustomEffects());
+				if(jaoium){
+					inventory.clear(n);
 				}
 			}
 		}
@@ -150,23 +167,10 @@ public class AntiJaoium implements Listener {
 				ItemStack hand = is[n];
 				if(hand.getType() == Material.POTION){
 					PotionMeta potion = (PotionMeta) hand.getItemMeta();
-					for (PotionEffect po : potion.getCustomEffects()) {
-						if(Jail.isJail(player)){
-							return;
-						}
-						if(po.getAmplifier() == 29){
-							// アウト
-							jaoium = true;
-						}
-						if(po.getAmplifier() == 125){
-							// アウト
-							jaoium = true;
-						}
-						if(jaoium){
-							enderchestinventory.clear(n);
-						}
+					jaoium = isjaoium(potion.getCustomEffects());
+					if(jaoium){
+						inventory.clear(n);
 					}
-
 				}
 			}
 			if(jaoium){
@@ -195,21 +199,9 @@ public class AntiJaoium implements Listener {
 			ItemStack hand = is[n];
 			if(hand.getType() == Material.POTION){
 				PotionMeta potion = (PotionMeta) hand.getItemMeta();
-				for (PotionEffect po : potion.getCustomEffects()) {
-					if(Jail.isJail(player)){
-						return;
-					}
-					if(po.getAmplifier() == 29){
-						// アウト
-						jaoium = true;
-					}
-					if(po.getAmplifier() == 125){
-						// アウト
-						jaoium = true;
-					}
-					if(jaoium){
-						inventory.clear(n);
-					}
+				jaoium = isjaoium(potion.getCustomEffects());
+				if(jaoium){
+					inventory.clear(n);
 				}
 			}
 		}
@@ -226,23 +218,10 @@ public class AntiJaoium implements Listener {
 				ItemStack hand = is[n];
 				if(hand.getType() == Material.POTION){
 					PotionMeta potion = (PotionMeta) hand.getItemMeta();
-					for (PotionEffect po : potion.getCustomEffects()) {
-						if(Jail.isJail(player)){
-							return;
-						}
-						if(po.getAmplifier() == 29){
-							// アウト
-							jaoium = true;
-						}
-						if(po.getAmplifier() == 125){
-							// アウト
-							jaoium = true;
-						}
-						if(jaoium){
-							enderchestinventory.clear(n);
-						}
+					jaoium = isjaoium(potion.getCustomEffects());
+					if(jaoium){
+						inventory.clear(n);
 					}
-
 				}
 			}
 			if(jaoium){
@@ -273,21 +252,9 @@ public class AntiJaoium implements Listener {
 			ItemStack hand = is[n];
 			if(hand.getType() == Material.POTION){
 				PotionMeta potion = (PotionMeta) hand.getItemMeta();
-				for (PotionEffect po : potion.getCustomEffects()) {
-					if(Jail.isJail(player)){
-						return;
-					}
-					if(po.getAmplifier() == 29){
-						// アウト
-						jaoium = true;
-					}
-					if(po.getAmplifier() == 125){
-						// アウト
-						jaoium = true;
-					}
-					if(jaoium){
-						inventory.clear(n);
-					}
+				jaoium = isjaoium(potion.getCustomEffects());
+				if(jaoium){
+					inventory.clear(n);
 				}
 			}
 		}
@@ -304,23 +271,10 @@ public class AntiJaoium implements Listener {
 				ItemStack hand = is[n];
 				if(hand.getType() == Material.POTION){
 					PotionMeta potion = (PotionMeta) hand.getItemMeta();
-					for (PotionEffect po : potion.getCustomEffects()) {
-						if(Jail.isJail(player)){
-							return;
-						}
-						if(po.getAmplifier() == 29){
-							// アウト
-							jaoium = true;
-						}
-						if(po.getAmplifier() == 125){
-							// アウト
-							jaoium = true;
-						}
-						if(jaoium){
-							enderchestinventory.clear(n);
-						}
+					jaoium = isjaoium(potion.getCustomEffects());
+					if(jaoium){
+						inventory.clear(n);
 					}
-
 				}
 			}
 			if(jaoium){
@@ -352,21 +306,9 @@ public class AntiJaoium implements Listener {
 			ItemStack hand = is[n];
 			if(hand.getType() == Material.POTION){
 				PotionMeta potion = (PotionMeta) hand.getItemMeta();
-				for (PotionEffect po : potion.getCustomEffects()) {
-					if(Jail.isJail(player)){
-						return;
-					}
-					if(po.getAmplifier() == 29){
-						// アウト
-						jaoium = true;
-					}
-					if(po.getAmplifier() == 125){
-						// アウト
-						jaoium = true;
-					}
-					if(jaoium){
-						inventory.clear(n);
-					}
+				jaoium = isjaoium(potion.getCustomEffects());
+				if(jaoium){
+					inventory.clear(n);
 				}
 			}
 		}
@@ -383,23 +325,10 @@ public class AntiJaoium implements Listener {
 				ItemStack hand = is[n];
 				if(hand.getType() == Material.POTION){
 					PotionMeta potion = (PotionMeta) hand.getItemMeta();
-					for (PotionEffect po : potion.getCustomEffects()) {
-						if(Jail.isJail(player)){
-							return;
-						}
-						if(po.getAmplifier() == 29){
-							// アウト
-							jaoium = true;
-						}
-						if(po.getAmplifier() == 125){
-							// アウト
-							jaoium = true;
-						}
-						if(jaoium){
-							enderchestinventory.clear(n);
-						}
+					jaoium = isjaoium(potion.getCustomEffects());
+					if(jaoium){
+						inventory.clear(n);
 					}
-
 				}
 			}
 			if(jaoium){
@@ -409,6 +338,18 @@ public class AntiJaoium implements Listener {
 		if(jaoium){
 			Bukkit.broadcastMessage("[jaoium_Checker] " + ChatColor.GREEN + "プレイヤー「" + player.getName() + "」からjaoiumと同等の性能を持つアイテムが検出されました。");
 			Jail.JailAdd(player, Bukkit.getOfflinePlayer("jaotan"), "jaoium所持", true);
+			event.setCancelled(true);
+		}
+	}
+	@EventHandler
+	public void OnBlockDispenseEvent(BlockDispenseEvent event){
+		Boolean jaoium = false;
+		ItemStack is = event.getItem();
+		if(is.getType() == Material.POTION){
+			PotionMeta potion = (PotionMeta) is.getItemMeta();
+			jaoium = isjaoium_indispenser(potion.getCustomEffects());
+		}
+		if(jaoium){
 			event.setCancelled(true);
 		}
 	}
