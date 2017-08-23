9d1c8dc5326d55298d7e28d65f22fce111b42fdf
tomachi <lego@aol.jp>
2017-08-23 14:48:11 +0900

    add: QPPEとDefaultは一部ブロックを破壊できないように

--- src/main/java/xyz/jaoafa/mymaid/EventHandler/OnBreak.java
+++ src/main/java/xyz/jaoafa/mymaid/EventHandler/OnBreak.java
@@ -1,7 +1,11 @@
 package xyz.jaoafa.mymaid.EventHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.CropState;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,29 +13,44 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.material.Crops;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
 public class OnBreak implements Listener {
 	JavaPlugin plugin;
+	List<Material> LDNoBreak = new ArrayList<Material>();
 	public OnBreak(JavaPlugin plugin) {
 		this.plugin = plugin;
+
+		LDNoBreak.add(Material.BARRIER);
+		LDNoBreak.add(Material.COMMAND);
+		LDNoBreak.add(Material.ITEM_FRAME);
 	}
 	@EventHandler
-	public void onBreak(BlockBreakEvent e){
-		if(e.getBlock().getWorld().getName().equalsIgnoreCase("Summer2017")){
+	public void onBreak(BlockBreakEvent event){
+		Player player = event.getPlayer();
+		if (!(player instanceof Player)) {
 			return;
 		}
-		Player player = e.getPlayer();
-    	if (!(player instanceof Player)) {
+		Block block = event.getBlock();
+		if(PermissionsEx.getUser(player).inGroup("QPPE") || PermissionsEx.getUser(player).inGroup("Default")){
+			// QPPEとDefaultは一部ブロックを破壊できない
+			if(LDNoBreak.contains(block.getType())){
+				event.setCancelled(true);
+			}
+		}
+
+		if(block.getWorld().getName().equalsIgnoreCase("Summer2017")){
 			return;
 		}
-		if(e.getBlock().getType() == Material.CROPS){
-			Crops block = (Crops)e.getBlock().getState().getData();
-			if(block.getState() == CropState.RIPE){
+		if(block.getType() == Material.CROPS){
+			Crops crop = (Crops) block.getState().getData();
+			if(crop.getState() == CropState.RIPE){
 				if(player.getItemInHand().getType() != Material.DIAMOND_SWORD &&
-					player.getItemInHand().getType() != Material.GOLD_SWORD &&
-					player.getItemInHand().getType() != Material.IRON_SWORD &&
-					player.getItemInHand().getType() != Material.STONE_SWORD &&
-					player.getItemInHand().getType() != Material.WOOD_SWORD){
-					e.setCancelled(true);
+						player.getItemInHand().getType() != Material.GOLD_SWORD &&
+						player.getItemInHand().getType() != Material.IRON_SWORD &&
+						player.getItemInHand().getType() != Material.STONE_SWORD &&
+						player.getItemInHand().getType() != Material.WOOD_SWORD){
+					event.setCancelled(true);
 				}
 			}
 		}
