10f8c0fb409a98fec402105f876fe9bd7e011af0
tomachi <lego@aol.jp>
2017-09-01 10:29:42 +0900

    add: 2017年8月イベントポイント配布処理追加

--- src/main/java/xyz/jaoafa/mymaid/EventHandler/OnSummer2017.java
+++ src/main/java/xyz/jaoafa/mymaid/EventHandler/OnSummer2017.java
@@ -6,11 +6,19 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,10 +28,11 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import xyz.jaoafa.mymaid.BugReport;
+import xyz.jaoafa.mymaid.Method;
 import xyz.jaoafa.mymaid.MyMaid;
+import xyz.jaoafa.mymaid.Pointjao;
 
 public class OnSummer2017 implements Listener {
 	JavaPlugin plugin;
@@ -34,6 +43,8 @@ public class OnSummer2017 implements Listener {
 	static Map<String, Long> summer2017online = new HashMap<String, Long>();
 	// プレイヤー:ワールドログイン時間
 	static Map<String, Long> summer2017counter = new HashMap<String, Long>();
+	// プレイヤー:ポイント配布が行われているか
+	static Map<String, Boolean> summer2017distribution = new HashMap<String, Boolean>();
 
 	@EventHandler
 	public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent event){
@@ -41,28 +52,89 @@ public class OnSummer2017 implements Listener {
 		if(event.getFrom().getName().equals(player.getWorld().getName())){
 			return;
 		}
-		if(event.getFrom().getName().equalsIgnoreCase("Summer2017")){
-			// Summer2017からどこか - カウンターを停止
-			EndCounter(player);
-		}else if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
-			// どこかからSummer2017 - カウンターを開始
-			StartCounter(player);
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date August_start = format.parse("2017/08/01 00:00:00");
+			Date August_end = format.parse("2017/08/31 23:59:59");
+
+			Date September_start = format.parse("2017/09/01 00:00:00");
+			Date September_end = format.parse("2017/09/30 23:59:59");
+
+			if(Method.isPeriod(August_start, August_end)){
+				// 8月はオンライン時間計測
+				if(event.getFrom().getName().equalsIgnoreCase("Summer2017")){
+					// Summer2017からどこか - カウンターを停止
+					EndCounter(player);
+				}else if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					// どこかからSummer2017 - カウンターを開始
+					StartCounter(player);
+				}
+			}else if(Method.isPeriod(September_start, September_end)){
+				// 9月は配布
+				if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					PointDistribution(player);
+				}
+			}
+		} catch (ParseException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
+
 	}
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent event){
 		Player player = event.getPlayer();
-		if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
-			// Summer2017にJoin - カウンターを開始
-			StartCounter(player);
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date August_start = format.parse("2017/08/01 00:00:00");
+			Date August_end = format.parse("2017/08/31 23:59:59");
+
+			Date September_start = format.parse("2017/09/01 00:00:00");
+			Date September_end = format.parse("2017/09/30 23:59:59");
+
+			if(Method.isPeriod(August_start, August_end)){
+				// 8月はオンライン時間計測
+				if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					// Summer2017にJoin - カウンターを開始
+					StartCounter(player);
+				}
+			}else if(Method.isPeriod(September_start, September_end)){
+				// 9月は配布
+				if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					PointDistribution(player);
+				}
+			}
+		} catch (ParseException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
 	}
 	@EventHandler
 	public void onPlayerQuitEvent(PlayerQuitEvent event){
 		Player player = event.getPlayer();
-		if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
-			// Summer2017からQuit - カウンターを停止
-			EndCounter(player);
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date August_start = format.parse("2017/08/01 00:00:00");
+			Date August_end = format.parse("2017/08/31 23:59:59");
+
+			Date September_start = format.parse("2017/09/01 00:00:00");
+			Date September_end = format.parse("2017/09/30 23:59:59");
+
+			if(Method.isPeriod(August_start, August_end)){
+				// 8月はオンライン時間計測
+				if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					// Summer2017からQuit - カウンターを停止
+					EndCounter(player);
+				}
+			}else if(Method.isPeriod(September_start, September_end)){
+				// 9月は配布
+				if(player.getWorld().getName().equalsIgnoreCase("Summer2017")){
+					PointDistribution(player);
+				}
+			}
+		} catch (ParseException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
 	}
 
@@ -93,6 +165,10 @@ public class OnSummer2017 implements Listener {
 		}
 		long join = summer2017counter.get(uuid);
 		long now = System.currentTimeMillis();
+		if(now > Long.parseLong("1504191599000")){
+			// 2017/08/31 23:59:59より後
+			now = Long.parseLong("1504191599000");
+		}
 		long time = now - join;
 		long all = summer2017online.get(uuid);
 		summer2017online.put(uuid, all + time);
@@ -101,6 +177,57 @@ public class OnSummer2017 implements Listener {
 			SaveSummerData();
 		} catch (Exception e) {
 			return;
+		}
+	}
+
+	private boolean PointDistribution(Player player){
+		String uuid = player.getUniqueId().toString();
+		if(!summer2017online.containsKey(uuid)){
+			return false;
+		}
+		if(summer2017distribution.containsKey(uuid)){
+			if(summer2017distribution.get(uuid)){
+				// trueなら配布済み
+				return false;
+			}
+		}
+		long onlineTimeMills = summer2017online.get(uuid); // オンライン時間(ミリ秒)
+		int onlineTime = (int) (onlineTimeMills / 1000); // 秒に
+		onlineTime = onlineTime / 60; // 分に
+		onlineTime = onlineTime / 3; // 3分に(ポイント数)
+
+		int Rank = getPlayerRank(onlineTimeMills);
+
+		Pointjao.addjao(player, onlineTime, "2017年8月イベントのオンライン時間ポイント配布分");
+
+		Long counter = OnSummer2017.getSummerCounter(player);
+		int time = (int) (counter / 1000);
+
+		player.sendMessage("[Summer2017] " + ChatColor.GREEN + "2017年8月イベントのポイントを配布しました！");
+		player.sendMessage("[Summer2017] " + ChatColor.GREEN + "あなたの8月中サバイバルワールドオンライン時間: " + (time/3600) + "時間" + ((time % 3600) / 60) + "分" + (time % 60) + "秒 (" + time + "秒)");
+		player.sendMessage("[Summer2017] " + ChatColor.GREEN + "順位: " + Rank + "位");
+
+		summer2017distribution.put(uuid, true);
+
+		try {
+			SaveSummerData();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private int getPlayerRank(Long onlineTimeMills){
+		Map<String, Long> online = new HashMap<String, Long>(summer2017online);
+		List<Long> list = new ArrayList<Long>();
+		for(Entry<String, Long> one : online.entrySet()) {
+			list.add(one.getValue());
+		}
+		Collections.sort(list, Comparator.reverseOrder());
+		if(list.indexOf(onlineTimeMills) != -1){
+			return list.indexOf(onlineTimeMills) + 1;
+		}else{
+			return -1;
 		}
 	}
 
@@ -113,7 +240,7 @@ public class OnSummer2017 implements Listener {
 			}
 		}
 		if(summer2017online.containsKey(player.getUniqueId().toString())){
-			if(summer2017counter.get(player.getUniqueId().toString()) == -1){
+			if(!summer2017counter.containsKey(player.getUniqueId().toString()) || summer2017counter.get(player.getUniqueId().toString()) == -1){
 				return summer2017online.get(player.getUniqueId().toString());
 			}else{
 				Long now = System.currentTimeMillis() - summer2017counter.get(player.getUniqueId().toString());
@@ -140,8 +267,14 @@ public class OnSummer2017 implements Listener {
 			OnlineJSON.put(one.getKey(), one.getValue());
 		}
 
+		JSONObject DistributionJSON = new JSONObject();
+		for(Entry<String, Boolean> one : summer2017distribution.entrySet()) {
+			DistributionJSON.put(one.getKey(), one.getValue());
+		}
+
 		JSONObject ALLJSON = new JSONObject();
 		ALLJSON.put("Online", OnlineJSON);
+		ALLJSON.put("Distribution", DistributionJSON);
 
 		try{
 			JavaPlugin plugin = MyMaid.getJavaPlugin();
@@ -190,12 +323,17 @@ public class OnSummer2017 implements Listener {
 		JSONObject obj;
 		try {
 			obj = (JSONObject) parser.parse(json);
-		} catch (ParseException e1) {
+		} catch (org.json.simple.parser.ParseException e1) {
 			obj = new JSONObject();
 		}
 		if(obj.containsKey("Online")){
 			for(Entry<String, Long> one : (Set<Map.Entry<String, Long>>) ((JSONObject) obj.get("Online")).entrySet()){
 				summer2017online.put(one.getKey(), one.getValue());
+			}
+		}
+		if(obj.containsKey("Distribution")){
+			for(Entry<String, Boolean> one : (Set<Map.Entry<String, Boolean>>) ((JSONObject) obj.get("Distribution")).entrySet()){
+				summer2017distribution.put(one.getKey(), one.getValue());
 			}
 		}
 		Loaded = true;
