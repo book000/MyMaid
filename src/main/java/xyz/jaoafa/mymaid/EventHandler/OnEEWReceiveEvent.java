157d83b7644008e4311532a9c03ad4b8174fab74
tomachi <lego@aol.jp>
2017-09-10 18:02:17 +0900

    change: EEW受信時の通知連絡調整

--- src/main/java/xyz/jaoafa/mymaid/EventHandler/OnEEWReceiveEvent.java
+++ src/main/java/xyz/jaoafa/mymaid/EventHandler/OnEEWReceiveEvent.java
@@ -16,53 +16,58 @@ public class OnEEWReceiveEvent implements Listener {
 		this.plugin = plugin;
 	}
 	@SuppressWarnings("deprecation")
-    @EventHandler
+	@EventHandler
 	public void onEEWReceiveEvent(EEWReceiveEvent e){
-    	//取得するもん取ってwebサーバーと連携
-    	// RTかどうか
-    	if(e.eew.isRetweet){
-    		//Bukkit.getLogger().info("[MyEEW] 地震情報を受信しましたが無視されました。(RT)");
-    		return;
-    	}
-    	String[] data = e.getEewArray();
+		//取得するもん取ってwebサーバーと連携
+		// RTかどうか
+		if(e.eew.isRetweet){
+			//Bukkit.getLogger().info("[MyEEW] 地震情報を受信しましたが無視されました。(RT)");
+			return;
+		}
+		String[] data = e.getEewArray();
 
-    	String separator = ",";
+		String separator = ",";
 
-    	StringBuilder sb = new StringBuilder();
-    	for (String str : data) {
-    	    if (sb.length() > 0) {
-    	        sb.append(separator);
-    	    }
-    	    sb.append(str);
-    	}
+		StringBuilder sb = new StringBuilder();
+		for (String str : data) {
+			if (sb.length() > 0) {
+				sb.append(separator);
+			}
+			sb.append(str);
+		}
 
-    	String result = Method.url_jaoplugin("eew", "u=" + sb.toString().replaceAll(" ", "_"));
-    	//result
-    	if(result.equalsIgnoreCase("null")){
-    		//Bukkit.getLogger().info("[MyEEW] 地震情報を受信しましたが無視されました。(NetworkReturn)");
-    		return;
-    	}else if(result.equalsIgnoreCase("print")){
-    		String kairiku, keihou;
-    		if(data[13].equalsIgnoreCase("0")){
-    			kairiku = "陸";
-    		}else{
-    			kairiku = "海";
-    		}
-    		if(data[14].equalsIgnoreCase("0")){
-    			keihou = "無";
-    		}else{
-    			keihou = "有";
-    		}
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "------------ 地震速報 ------------");
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "地震ID: " + data[5]);
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "発生時刻/発表時刻: " + data[6] + "/" + data[2]);
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源地(海陸判定): " + data[9] + " (" + kairiku + ")");
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "最大震度(マグニチュード): " + data[12] + " (" + data[11] + "M)");
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源の深さ: " + data[10] + "km");
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "警報有無: " + keihou);
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源に近い住民の方は安全に確認して必要であれば避難して下さい。");
-    		Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "----------------------------------");
-    		Bukkit.getLogger().info("[MyEEW] 地震情報を受信し、全プレイヤーに送信しました。。");
-    	}
-    }
+		String result = Method.url_jaoplugin("eew", "u=" + sb.toString().replaceAll(" ", "_"));
+		//result
+		if(result.equalsIgnoreCase("null")){
+			//Bukkit.getLogger().info("[MyEEW] 地震情報を受信しましたが無視されました。(NetworkReturn)");
+			return;
+		}else if(result.equalsIgnoreCase("print")){
+			String kairiku, keihou, DataType;
+			if(data[3].equalsIgnoreCase("8") || data[3].equalsIgnoreCase("9")){
+				DataType = "最終報";
+			}else{
+				DataType = "速報";
+			}
+			if(data[13].equalsIgnoreCase("0")){
+				kairiku = "陸";
+			}else{
+				kairiku = "海";
+			}
+			if(data[14].equalsIgnoreCase("0")){
+				keihou = "無";
+			}else{
+				keihou = "有";
+			}
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "------------ 地震速報 (" + DataType + ") ------------");
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "地震ID: " + data[5]);
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "発生時刻/発表時刻: " + data[6] + "/" + data[2]);
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源地(海陸判定): " + data[9] + " (" + kairiku + ")");
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "最大震度(マグニチュード): " + data[12] + " (" + data[11] + "M)");
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源の深さ: " + data[10] + "km");
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "警報有無: " + keihou);
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "震源に近い住民の方は安全に確認して必要であれば避難して下さい。");
+			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "MyEEW" + ChatColor.RESET + "] " + ChatColor.RED + "----------------------------------");
+			Bukkit.getLogger().info("[MyEEW] 地震情報を受信し、全プレイヤーに送信しました。。");
+		}
+	}
 }
