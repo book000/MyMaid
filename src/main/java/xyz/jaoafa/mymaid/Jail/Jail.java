b1575140da106afdca31ad6f6cbe9d027f5bb0de
tomachi <lego@aol.jp>
2017-10-28 14:16:58 +0900

    add: #22 jaotanによる投獄時のみ画像送信

--- src/main/java/xyz/jaoafa/mymaid/Jail/Jail.java
+++ src/main/java/xyz/jaoafa/mymaid/Jail/Jail.java
@@ -215,6 +215,9 @@ public class Jail {
 		Method.url_jaoplugin("eban", "p=" + player.getName() +"&u=" + player.getUniqueId().toString() +"&b=" + banned_by.getName() + "&r=" + reason + "&nosend");
 		Bukkit.broadcastMessage("[JAIL] " + ChatColor.GREEN + "プレイヤー:「" + player.getName() + "」を「" + reason + "」という理由で牢獄リストに追加しました。");
 		Discord.send("223582668132974594", "***EBan[追加]***: プレイヤー「" + player.getName() +"」が「" + banned_by.getName() +"」によって「" + reason + "」という理由でJailリストに追加されました。");
+		if(banned_by.getName().equalsIgnoreCase("jaotan")){
+			Discord.Filesend(MyMaid.getJavaPlugin().getDataFolder() + File.separator + "jailadd.png");
+		}
 		JailBackupSaveTxt(player.getName(), JailType.ADD, banned_by.getName(), reason);
 		return true;
 	}
