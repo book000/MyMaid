de99cab8ccbb97beac2842b105fc5332d6259396
tomachi <lego@aol.jp>
2017-12-06 15:55:08 +0900

    change: とりあえずDiscordへ送信はできるように

--- src/main/java/xyz/jaoafa/mymaid/Discord/Discord.java
+++ src/main/java/xyz/jaoafa/mymaid/Discord/Discord.java
@@ -1,7 +1,13 @@
 package xyz.jaoafa.mymaid.Discord;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,8 +17,11 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.regex.Pattern;
 
+import javax.net.ssl.HttpsURLConnection;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+import org.json.simple.JSONObject;
 
 import sx.blah.discord.Discord4J;
 import sx.blah.discord.api.ClientBuilder;
@@ -34,7 +43,7 @@ import xyz.jaoafa.mymaid.MySQL;
 
 public class Discord {
 	JavaPlugin plugin;
-	String token;
+	static String token;
 	public static IDiscordClient client = null;
 	static IGuild guild = null;
 	static IChannel channel = null;
@@ -106,6 +115,109 @@ public class Discord {
 	}
 
 	public static boolean send(String message){
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Content-Type", "application/json");
+		headers.put("Authorization", "Bot " + token);
+		headers.put("User-Agent", "DiscordBot (https://jaoafa.com, v0.0.1)");
+
+		Map<String, String> contents = new HashMap<String, String>();
+		headers.put("content", message);
+		return postHttpJsonByJson("https://discordapp.com/api/channels/250613942106193921/messages", headers, contents);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean postHttpJsonByJson(String address, Map<String, String> headers, Map<String, String> contents){
+		StringBuilder builder = new StringBuilder();
+		try{
+			URL url = new URL(address);
+
+			/*TrustManager[] tm = { new X509TrustManager() {
+				@Override
+				public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+						throws CertificateException {
+					// TODO 自動生成されたメソッド・スタブ
+
+				}
+				@Override
+				public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+						throws CertificateException {
+					// TODO 自動生成されたメソッド・スタブ
+
+				}
+				@Override
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					// TODO 自動生成されたメソッド・スタブ
+					return null;
+				}
+			}};
+			SSLContext sslcontext = SSLContext.getInstance("SSL");
+			sslcontext.init(null, tm, null);
+			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+			 */
+			HttpsURLConnection connect = (HttpsURLConnection)url.openConnection();
+			connect.setRequestMethod("POST");
+			// connect.setSSLSocketFactory(sslcontext.getSocketFactory());
+			if(headers != null){
+				for(Map.Entry<String, String> header : headers.entrySet()){
+					connect.setRequestProperty(header.getKey(), header.getValue());
+				}
+			}
+
+			connect.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connect.getOutputStream());
+			//List<String> list = new ArrayList<>();
+			JSONObject paramobj = new JSONObject();
+			for(Map.Entry<String, String> content : contents.entrySet()){
+				//list.add(content.getKey() + "=" + content.getValue());
+				paramobj.put(content.getKey(), content.getValue());
+			}
+			//String param = implode(list, "&");
+			out.write(paramobj.toJSONString());
+			out.close();
+
+			connect.connect();
+
+			if(connect.getResponseCode() != HttpURLConnection.HTTP_OK){
+				InputStream in = connect.getErrorStream();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+				in.close();
+				connect.disconnect();
+
+				//BugReport.report(new IOException(builder.toString()));
+				return false;
+			}
+
+			InputStream in = connect.getInputStream();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+			in.close();
+			connect.disconnect();
+			//JSONParser parser = new JSONParser();
+			//Object obj = parser.parse(builder.toString());
+			//JSONObject json = (JSONObject) obj;
+			return true;
+		}catch(Exception e){
+			//BugReport.report(e);
+			return false;
+		}
+	}
+
+/*
+	public static boolean send(String message){
 		if(channel == null){
 			return false;
 		}
@@ -129,7 +241,7 @@ public class Discord {
 		}
 		return true;
 	}
-
+*/
 	public static boolean Filesend(IChannel channel, String path){
 		File file = new File(path);
 		if(!file.exists()){
@@ -264,6 +376,18 @@ public class Discord {
 		return true;
 	}
 
+	public static boolean send(String channelid, String message){
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Content-Type", "application/json");
+		headers.put("Authorization", "Bot " + token);
+		headers.put("User-Agent", "DiscordBot (https://jaoafa.com, v0.0.1)");
+
+		Map<String, String> contents = new HashMap<String, String>();
+		headers.put("content", message);
+		return postHttpJsonByJson("https://discordapp.com/api/channels/" + channelid + "/messages", headers, contents);
+	}
+
+	/*
 	public static boolean send(String channelid_or_name, String message){
 		IChannel channel = null;
 		for (IChannel one : guild.getChannels()) {
@@ -287,6 +411,7 @@ public class Discord {
 		send(channel, message);
 		return true;
 	}
+	*/
 
 	public static boolean send(String channelid_or_name, String message, EmbedObject embed){
 		IChannel channel = null;
